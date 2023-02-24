e1ddae3b162da4e6cec8fb72a9ffdfef0da82463
bosstnp <boss.32560@gmail.com>
2023-02-24 13:37:28 +0700

    feat: filter undone problems

--- contentScript.js
+++ contentScript.js
@@ -1,50 +1,75 @@
 const panel = document.getElementsByClassName("col-md-7")[0];
-const bottomPanel = panel.getElementsByClassName("table table-striped table-condensed")[0];
-const tasks = bottomPanel.getElementsByTagName("tbody")[0].getElementsByTagName("tr");
+const bottomPanel = panel.getElementsByClassName(
+  "table table-striped table-condensed"
+)[0];
+const tasks = bottomPanel
+  .getElementsByTagName("tbody")[0]
+  .getElementsByTagName("tr");
+const fileDownloadPanel = document.getElementsByClassName("list-group")[0];
+const fileDownload = fileDownloadPanel
+  .getElementsByTagName("li")[0]
+  .getElementsByTagName("li");
+
+let downloads = [];
 
 // Remove grey background
 bottomPanel.className = "table table-condensed";
 
 // Load search bar
 const loadHTML = async () => {
-  return await fetch(chrome.runtime.getURL("container.html")).then(res => res.text());
-}
+  return await fetch(chrome.runtime.getURL("container.html")).then((res) =>
+    res.text()
+  );
+};
 
 const attachSearchBar = async () => {
-  let searchBarContainer = document.getElementsByClassName("search-bar-container")[0];
+  let searchBarContainer = document.getElementsByClassName(
+    "search-bar-container"
+  )[0];
 
   if (!searchBarContainer) {
     searchBarContainer = document.createElement("div");
     searchBarContainer.className = "search-bar-container";
-  
+
     let content = await loadHTML();
     if (!content) content = "Error loading extension!";
     searchBarContainer.innerHTML = content;
   }
-  
+
   panel.insertBefore(searchBarContainer, bottomPanel);
 
   const searchBar = document.getElementById("search-bar");
-  searchBar.addEventListener("input", (e) => { updateTasks(searchBar.value.trim().toLowerCase()); });
+  searchBar.addEventListener("input", (e) => {
+    updateTasks(searchBar.value.trim().toLowerCase());
+  });
 };
 
 const updateTasks = (filter) => {
+  let [query, option] = filter.split(";");
+
+  let undone = option !== undefined && option.includes("undone");
+
   for (let i of tasks) {
-    let [ name, fullName, subs, results, editBtn ] = i.getElementsByTagName("td");
+    let [name, fullName, subs, results, editBtn] = i.getElementsByTagName("td");
     let classes = "";
 
     let n = name.innerHTML.trim().toLowerCase();
     let fn = fullName.innerText.split("\n")[0].trim().toLowerCase();
 
-    if (!n.includes(filter) && !fn.includes(filter)) classes += "hide ";
+    if (
+      (!n.includes(query) && !fn.includes(query)) ||
+      (undone && parseInt(results.innerText.split("score: ")[1]) == 100)
+    )
+      classes += "hide ";
 
     i.className = classes + "task ";
   }
-}
+};
 
-const colorCases = () => {
+const processTask = () => {
+  let index = 1;
   for (let i of tasks) {
-    let [ name, fullName, subs, results, editBtn ] = i.getElementsByTagName("td");
+    let [name, fullName, subs, results, editBtn] = i.getElementsByTagName("td");
     let div = results.getElementsByTagName("tt")[0];
 
     if (!div) continue;
@@ -53,14 +78,20 @@ const colorCases = () => {
     let newText = "";
 
     for (let i = 0; i < cases.length; i++) {
-      newText += `<span class="${(cases[i] !== "P") ? "wrong" : "pass"}">${cases[i]}</span>`; 
+      if (cases[i] === "]" || cases[i] === "[") {
+        newText += cases[i];
+      } else {
+        newText += `<span class="${cases[i] === "P" ? "pass" : "wrong"}">${
+          cases[i]
+        }</span>`;
+      }
     }
 
     div.innerHTML = newText;
   }
-}
+};
 
 (() => {
   attachSearchBar();
-  colorCases();
+  processTask();
 })();
